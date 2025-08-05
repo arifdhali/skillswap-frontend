7c0da9c4d31e5eb82fd3cf725ea0e2db2b786e82
Arif Dhali <arifdhali98@gmail.com>
2025-08-06 00:07:50 +0530

    user router and page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Contact } from './pages/contact/contact';
 import { Pricing } from './pages/pricing/pricing';
+import { User } from './user/user/user';
 
 export const routes: Routes = [
     {path:"",component:Home},
     {path:"login",component:Login},
     {path:"register",component:Register},
+    {path:"users",component:User},
     {path:"contact",component:Contact},
+    // {path"user"},
     {path:"pricing",component:Pricing},
 ];
